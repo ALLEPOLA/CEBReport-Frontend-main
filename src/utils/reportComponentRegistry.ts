cda3d7b27a110454342468d8ff1fe2317a0d7236
bhagyanawarathna <bhagya0913@gmail.com>
2026-08-17 09:58:37 +0530

    Add Job Finished But Bill Not Generated Report

--- src/utils/reportComponentRegistry.ts
+++ src/utils/reportComponentRegistry.ts
@@ -201,6 +201,7 @@ import PivIIPaidNotEnagizedReport from "../mainTopics/SMC-ManagementInfo/PIVIIPa
 import JobRegisterCCReport from "../mainTopics/SMC-ManagementInfo/JobRegisterCCReport";
 import JobRegCCNCReport from "../mainTopics/SMC-ManagementInfo/JobRegCCNCReport";
 import SMCAllApplicationReport from "../mainTopics/SMC-ManagementInfo/SMCAllApplicationReport.tsx";
+import JobFinBillNotGeneratedReport from "../mainTopics/SMC-ManagementInfo/JobFinBillNotGeneratedReport.tsx";
 
 //FIFO reports
 import IssueReceiptSummaryReport from "../mainTopics/FIFO/IssueReceiptSummaryReport.tsx";
@@ -466,7 +467,8 @@ export const reportComponentRegistry: ReportComponentRegistry = {
 	"cost center wise job register": JobRegisterCCReport,
 	"cost center wise new connection job register": JobRegCCNCReport,
 	"area wise smc all details": SMCAllApplicationReport,
-
+	"cost center wise job finished not paid contractor payment": JobFinBillNotGeneratedReport,
+	
 	//FIFO reports
 	"issue and receipt summary": IssueReceiptSummaryReport,
 	"quantity on hand material wise fifo": QuantityMatFIFOReport,
